51775a6676013c48d7b5f6c2d078efc1a92ef7db
kurostreet <kuro.street.hit@gmail.com>
2026-08-20 00:56:09 +0300

    fix new site

---
+++ config.js
@@ -0,0 +1,42 @@
+window.KONTRA_CONFIG = {
+  serverName: "KONTRA // SURVIVAL",
+  serverAddress: "45.136.205.162:49494",
+  maxPlayers: 16,
+  statusEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/status",
+  statusRefreshMs: 30000,
+  statusBackoffMaxMs: 120000,
+  statusLeaderLeaseMs: 45000,
+  leaderboardRefreshMs: 60000,
+  staleAfterMs: 60000,
+  publicUrl: "https://kontra-fps.ru",
+  portals: {
+    zm: "https://kontra-fps.ru/",
+    ze: "https://wertyosluakontra.pages.dev/"
+  },
+  servers: {
+    survival: "45.136.205.162:49494",
+    escape: "31.25.243.157:49494"
+  },
+  deepLinks: {
+    zm: "kontra://join/zm",
+    ze: "kontra://join/ze"
+  },
+  authEnabled: true,
+  // Google Identity Services Web Client ID. Fill after creating OAuth Web Client in Google Cloud.
+  googleClientId: "288638880717-lbeskumhv9lao7u46nmo6o2r9encq73b.apps.googleusercontent.com",
+  authBaseEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/auth",
+  leaderboardEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/leaderboard",
+  playerEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/player",
+  controlBaseEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/control",
+  settingsBaseEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/settings",
+  lvlBootstrapEndpoint: "https://kontra-status-api.kuro-street-hit.workers.dev/lvl/bootstrap",
+  links: {
+    appStore: "https://apps.apple.com/ru/app/kontra-zombie-survival/id1660215198",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.gameplier.kontra",
+    kontraServerApk: "https://2d88cda2.nip.io/download/Kontra-Server.apk",
+    developer: "https://kontramobile.com/#/",
+    telegram: "https://t.me/kontra_fps_ru",
+    discord: "https://discord.gg/bj2xedeZ7K",
+    administration: "https://t.me/blackmodru"
+  }
+};
